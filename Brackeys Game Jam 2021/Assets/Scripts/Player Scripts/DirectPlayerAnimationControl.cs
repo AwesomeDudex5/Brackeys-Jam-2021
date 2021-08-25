35878d4848f08d98842b9e34956eb1e674d3576d
Comfy Cauldron <comfycauldron@gmail.com>
2021-08-24 20:01:26 -0700

    Improves Wizard Animation Controller
    Now has a blend tree handling idle + movement states. Just need to add branches for the hit, death, and spellcast animations.

---
+++ Brackeys Game Jam 2021/Assets/Scripts/Player Scripts/DirectPlayerAnimationControl.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/** Just for me to test the BlendTree really, 
+ *  but can use the same strategy for the actual controller */
+public class DirectPlayerAnimationControl : MonoBehaviour
+{
+    private const string X_LABEL = "X";
+    private const string Y_LABEL = "Y";
+    
+    public float x;
+    public float y;
+
+    private Animator controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = transform.GetChild(0).GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        controller.SetFloat(X_LABEL, x);
+        controller.SetFloat(Y_LABEL, y);
+    }
+}
