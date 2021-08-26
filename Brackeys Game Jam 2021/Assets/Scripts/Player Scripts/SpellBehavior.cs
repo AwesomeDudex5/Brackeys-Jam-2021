74450c1c4101cf7a2395a3baa15b0c964aca5125
ayden <aydenocceas@gmail.com>
2021-08-26 19:33:27 -0400

    Auto stash before merge of "main" and "origin/main"

--- Brackeys Game Jam 2021/Assets/Scripts/Player Scripts/SpellBehavior.cs
+++ Brackeys Game Jam 2021/Assets/Scripts/Player Scripts/SpellBehavior.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SpellBehavior : MonoBehaviour
 {
+    public bool isProjectile;
+    private Coroutine moveToTarget;
+    public Vector3 target;
+    public float speed;
 
     public bool hasBurn;
     public bool hasWet;
@@ -22,7 +26,16 @@ public class SpellBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(isProjectile) moveToTarget = StartCoroutine(MoveToTarget());
+    }
+
+    IEnumerator MoveToTarget()
+    {
+        while ((Vector3)transform.position != target)
+        {
+            transform.position = Vector3.MoveTowards((Vector3)transform.position, target, speed * Time.deltaTime);
+            yield return null;
+        }
     }
 
     // Update is called once per frame
--- Brackeys Game Jam 2021/Assets/Scripts/Player Scripts/SpellManager.cs
+++ Brackeys Game Jam 2021/Assets/Scripts/Player Scripts/SpellManager.cs
@@ -2,7 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 public class Spell
 {
     public GameObject spellPrefab;
@@ -10,10 +13,28 @@ public class Spell
     public float currentCooldown;
 
     public Spell(GameObject _spellPrefab, float _cooldownTime)
+<<<<<<< Updated upstream
     {
         spellPrefab = _spellPrefab;
         cooldownTime = _cooldownTime;
         currentCooldown = _cooldownTime;
+=======
+    {
+        spellPrefab = _spellPrefab;
+        cooldownTime = _cooldownTime;
+        currentCooldown = 0;
+    }
+
+    public bool isCooled()
+    {
+        if (currentCooldown <= 0) return true;
+        else return false;
+     }
+    // Cooldown Code would go here
+    public void cooldownStart()
+    {
+
+>>>>>>> Stashed changes
     }
 }
 
@@ -21,9 +42,28 @@ public class SpellManager : MonoBehaviour
 {
 
     public List<Spell> spellList = new List<Spell>();
+    public GameObject testSpellprefab;
+
+    //test spell
+    public void Start()
+    {
+        spellList.Add(new Spell(testSpellprefab, 150.0f));
+    }
+
+    public void Update()
+    {
+        ActivateSpell(spellList[0], new Vector3(6, 5, 10));
+    }
 
     public void ActivateSpell(Spell targetSpell, Vector3 targetLocation)
     {
-        GameObject newPrefab = Instantiate(targetSpell.spellPrefab, gameObject.transform);
+        if(targetSpell.isCooled())
+        {
+            GameObject newPrefab = Instantiate(targetSpell.spellPrefab, gameObject.transform.position, Quaternion.identity);
+            newPrefab.GetComponent<SpellBehavior>().target = targetLocation;
+            targetSpell.currentCooldown = targetSpell.cooldownTime;
+            targetSpell.cooldownStart();
+        }
+        Debug.Log("SpellChecked");
     }
 }
